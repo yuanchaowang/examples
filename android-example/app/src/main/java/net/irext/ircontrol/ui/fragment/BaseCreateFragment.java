31b9e3f0f9a95cf9262b2500274fd63f05b95cd4
strawmanbobi <strawmanbobi@163.com>
2018-11-18 13:10:27 +0800

    updated SDK version to 27

--- android-example/app/src/main/java/net/irext/ircontrol/ui/fragment/BaseCreateFragment.java
+++ android-example/app/src/main/java/net/irext/ircontrol/ui/fragment/BaseCreateFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import net.irext.ircontrol.ui.activity.CreateActivity;
 import net.irext.ircontrol.utils.MessageUtil;
 
